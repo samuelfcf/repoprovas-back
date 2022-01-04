0c8066a40b69448732390f4f369a7c0a30615085
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-01-04 10:04:46 -0300

    prepara revisão

--- src/routes/professor.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import ProfessorController from '../controllers/ProfessorController';
-
-const professorRouter = Router();
-
-professorRouter.get('/', ProfessorController.find);
-
-export default professorRouter;
